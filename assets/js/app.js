226d062d8da43756b72b6b9ce38b157def163c34
Emilio Tirado <emilio.tirado57@gmail.com>
2014-08-06 11:28:43 -0430

    listo el tutorial de ui-routes

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,11 +31,32 @@ mooseJs.config(function($stateProvider, $urlRouterProvider){
 		})
 
 		.state('about', {
-
+			url: '/about',
+			views : {
+				'': { templateUrl : 'templates/partial2.html'},
+				'columnOne@about' : { template : 'Look I am a column!'},
+				'columnTwo@about' : {
+					templateUrl : 'templates/table-data.html',
+					controller : 'scotchController'
+				}
+			}
 		});
 });
-// config(['$routeProvider', function($routeProvider) {
-//   $routeProvider.when('/view1', {templateUrl: 'templates/partial1.html', controller: 'MyCtrl1'});
-//   $routeProvider.when('/view2', {templateUrl: 'templates/partial2.html', controller: 'MyCtrl2'});
-//   $routeProvider.otherwise({redirectTo: '/view1'});
-// }]);
+
+mooseJs.controller('scotchController', function($scope){
+	$scope.message = 'test';
+	$scope.scotches = [
+		{
+			name: 'Macalan 12',
+			price: 50
+		},
+		{
+			name: 'Chivas Regal Royal Salute',
+			price: 10000
+		},
+		{
+			name: 'Glenfiddich 1937',
+			price: 20000
+		}
+	];
+});
