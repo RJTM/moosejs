8dca049f7f23861273a50aa0e6efb8b94fe42a34
Ricardo Montero <rjtm1992@gmail.com>
2014-09-19 18:33:24 -0430

    Testcase Controller, seed data for judgehost subtask and testcase

---
+++ api/controllers/TestcaseController.js
@@ -0,0 +1,11 @@
+/**
+ * TestcaseController
+ *
+ * @description :: Server-side logic for managing Testcases
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
