a68e129400be88f7912b1129ac287b29b0aeb906
Ricardo Montero <rjtm1992@gmail.com>
2014-09-25 20:00:49 -0430

    Angular modularization, navController

---
+++ assets/js/moose.module.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+angular.module('mooseJs', [
+
+	'mooseJs.admin',
+	'mooseJs.common',
+	'mooseJs.jury',
+	'mooseJs.public',
+	'mooseJs.staff',
+	'mooseJs.team',
+
+
+ 	'ui.router', 'ngResource','ui.bootstrap','ncy-angular-breadcrumb'
+]).run(function($rootScope, $state, Auth){
+	$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+		if(!Auth.authorize(toState.data.access)){
+			event.preventDefault();
+			$state.go('public.login');
+		}
+	});
+});
