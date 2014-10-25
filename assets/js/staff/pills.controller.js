290cc273bbde3be19a337c519bda37c345fcc599
Emilio Tirado <emilio.tirado57@gmail.com>
2014-10-25 17:46:29 -0430

    registering badges with sockets to receive new balloons and prints events

--- assets/js/staff/pills.controller.js
+++ assets/js/staff/pills.controller.js
@@ -4,4 +4,16 @@ angular.module('mooseJs.staff')
 	.controller('staff.PillsController', ["$scope", "socket", function($scope, socket){
 		$scope.prints = 0;
 		$scope.balloons = 0;
+
+		console.log('register sockets');
+		socket.get('/print');
+		socket.on('print', function(message){
+			$scope.prints++;
+		});
+
+		socket.get('/balloon');
+		socket.on('balloon', function(message){
+			console.log('balloon event');
+			$scope.balloons++;
+		});
 	}]);
