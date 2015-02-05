936ccbca241c4256d596f1eeb88b3afed919bb64
Ricardo Montero <rjtm1992@gmail.com>
2015-02-04 19:41:04 -0430

    Calculating time of submissions, login view fix

--- api/controllers/RunController.js
+++ api/controllers/RunController.js
@@ -14,32 +14,38 @@
  	**/
  	
  	submit: function(req,res) {
- 		var time = req.param('time');
  		var owner = req.token.id;
  		var task = req.param('task');
  		var source = req.file('source');
- 		var language = req.param('language')
- 		RunService.uploadSourceFile(task, owner, source, function(err, sourceUrl){
- 			if(err) return res.serverError(err);
- 			Run.create({
- 				time: time,
- 				owner: owner,
- 				task: task,
- 				language: language,
- 				source: sourceUrl
- 			}).exec(function(err, result){
- 				if(err) return res.json(500, err);
- 				JudgeService.dispatchRun(result);
- 				Run.findOne({id: result.id}).populate('owner').populate('task').exec(function(err, data){
- 					if(err){
- 						Run.publishCreate(result);
- 						return res.json(result);
- 					} 
- 					Run.publishCreate(data);
- 					return res.json(data);
+ 		var language = req.param('language');
+
+ 		Task.findOne(task).populate('contest').exec(function(err, fullTask){
+ 			var now = new Date();
+ 			var contestTime = new Date(fullTask.contest.startTime)
+ 			var time = parseInt((now - contestTime)/60000);
+ 			RunService.uploadSourceFile(task, owner, source, function(err, sourceUrl){
+ 				if(err) return res.serverError(err);
+ 				Run.create({
+ 					time: time,
+ 					owner: owner,
+ 					task: task,
+ 					language: language,
+ 					source: sourceUrl
+ 				}).exec(function(err, result){
+ 					if(err) return res.json(500, err);
+ 					JudgeService.dispatchRun(result);
+ 					Run.findOne({id: result.id}).populate('owner').populate('task').exec(function(err, data){
+ 						if(err){
+ 							Run.publishCreate(result);
+ 							return res.json(result);
+ 						} 
+ 						Run.publishCreate(data);
+ 						return res.json(data);
+ 					});
  				});
  			});
  		});
+ 		
 
  	},
 
