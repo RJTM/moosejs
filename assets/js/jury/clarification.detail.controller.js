5e5edad91c012c5d66d1417e785ba92702eb74ee
Emilio Tirado <emilio.tirado57@gmail.com>
2015-02-07 23:16:27 -0430

    adding jury info to clarification response

--- assets/js/jury/clarification.detail.controller.js
+++ assets/js/jury/clarification.detail.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('mooseJs.team')
-.controller('jury.ClarificationDetailController', ["$scope", "$modalInstance", "socket", "clarification", function($scope, $modalInstance, socket, clarification){
+.controller('jury.ClarificationDetailController', ["$scope", "$modalInstance", "socket", "clarification", "CurrentUser", function($scope, $modalInstance, socket, clarification, CurrentUser){
 	$scope.clarification = clarification;
 
 	$scope.ok = function(){
@@ -9,6 +9,7 @@ angular.module('mooseJs.team')
 	}
 
 	$scope.send = function(){
+		$scope.clarification.jury = CurrentUser.user().id;
 		socket.post('/clarification/' + $scope.clarification.id,
 			$scope.clarification,
 			function(data){
