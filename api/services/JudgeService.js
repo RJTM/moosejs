f23c6c778a3d3012667c8edbad159e2580dc327b
Ricardo Montero <rjtm1992@gmail.com>
2015-02-15 02:28:07 -0430

    Rejudge

--- api/services/JudgeService.js
+++ api/services/JudgeService.js
@@ -1,52 +1,66 @@
 'use strict';
 
 module.exports = {
-	dispatchRun: function(run){
-        /*
-        1.- Receive new Run
-        2.- Create new Grade with default status pending 
-        3.- Check for available Judgehosts
-        4.- If subscriber available: 
-                Send Run to that sub and change Grade status
-        5.- Save Grade
-        */
-        var subscribers = sails.sockets.subscribers('newSubmissions');
-        var grade = {
-        	run: run.id,
-        	task: run.task
-        };
-        if(subscribers.length > 0){
-        	var judgehostToSend = subscribers[0];
+        dispatchRun: function(run){
+            /*
+            1.- Receive new Run
+            2.- Create new Grade with default status pending 
+            3.- Check for available Judgehosts
+            4.- If subscriber available: 
+                    Send Run to that sub and change Grade status
+            5.- Save Grade
+            */
+            var subscribers = sails.sockets.subscribers('newSubmissions');
+            var grade = {
+            	run: run.id,
+            	task: run.task
+            };
+            if(subscribers.length > 0){
+            	var judgehostToSend = subscribers[0];
 
-        	Grade.create(grade).exec(function(err, result){
-        		if(err){
-        			sails.log.error(err);
-        			return;   
-        		}
-                        Grade.publishCreate(result);
-        		Grade.findOne({id: result.id}).populate('run').populate('task').exec(function(err, gradeToSend){
-        			if(err){
-	        			sails.log.error(err);
-	        			return;   
-	        		}
-        			GradeService.fillGradeData(gradeToSend, function(err, finalGrade){
-        				if(err){
-		        			sails.log.error(err);
-		        			return;   
-		        		}
-        				sails.sockets.emit(judgehostToSend, 'submission', finalGrade);
-        			});
-        		});
-        	});
+            	Grade.create(grade).exec(function(err, result){
+            		if(err){
+            			sails.log.error(err);
+            			return;   
+            		}
+                            Grade.publishCreate(result);
+            		Grade.findOne({id: result.id}).populate('run').populate('task').exec(function(err, gradeToSend){
+            			if(err){
+    	        			sails.log.error(err);
+    	        			return;   
+    	        		}
+            			GradeService.fillGradeData(gradeToSend, function(err, finalGrade){
+            				if(err){
+    		        			sails.log.error(err);
+    		        			return;   
+    		        		}
+            				sails.sockets.emit(judgehostToSend, 'submission', finalGrade);
+            			});
+            		});
+            	});
 
-        }else{
-        	Grade.create(grade).exec(function(err, result){
-        		if(err){
-        			sails.log.error(err);
-        			return;   
-        		}
-                        Grade.publishCreate(result);
-        	});
+            }else{
+            	Grade.create(grade).exec(function(err, result){
+            		if(err){
+            			sails.log.error(err);
+            			return;   
+            		}
+                            Grade.publishCreate(result);
+            	});
+            }
+        },
+
+        reJudgeRun: function(id){
+            Run.findOne(id).exec(function(err, run){
+                module.exports.dispatchRun(run);
+            });
+        },
+
+        reJudgeTask: function(id){
+            Task.findOne(id).populate('runs').exec(function(err, task){
+                task.runs.forEach(function(run){
+                    module.exports.reJudgeRun(run.id);
+                });
+            });
         }
-    }
 }
