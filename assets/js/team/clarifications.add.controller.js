a67d5c2c3976119989cdb050d1900a0cd478889b
Emilio Tirado <emilio.tirado57@gmail.com>
2015-02-15 00:47:39 -0430

    removing token from requests

--- assets/js/team/clarifications.add.controller.js
+++ assets/js/team/clarifications.add.controller.js
@@ -3,8 +3,7 @@
 angular.module('mooseJs.team')
 .controller('team.ClarificationsAddController', ["$scope", "$sce", "$state", "socket", "LocalService", function($scope, $sce, $state, socket, LocalService){
 	$scope.subjects = [{name: 'General Issue'}];
-	//TODO: Get token out of here
-	socket.get('/task/contest', {token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiaWF0IjoxNDE0Mjg0MTQ0fQ.enUSSR4pWilw5hyGEc1yWZf5ZqyvK_BBqPFxchMk14o'} ,function(data){
+	socket.get('/task/contest', function(data){
 		$scope.subjects.push.apply($scope.subjects, data);
 	});
 
@@ -12,8 +11,6 @@ angular.module('mooseJs.team')
 		return $sce.trustAsHtml(value);
 	};
 	$scope.submitClarification = function(){
-		//TODO: Remove token from data
-		$scope.clarification.token = angular.fromJson(LocalService.get('auth_token')).token;
 		socket.post('/clarification', $scope.clarification, function(){
 			$scope.clarification = {};
 			$state.go('team.clarifications');
