e2e85acca6d713dd28ba4eb26721bc7f98ae252d
Emilio Tirado <emilio.tirado57@gmail.com>
2015-02-15 16:39:08 -0430

    filter tasks by contest in jury view

--- assets/js/jury/tasks.controller.js
+++ assets/js/jury/tasks.controller.js
@@ -1,5 +1,16 @@
 'use strict';
 angular.module('mooseJs.jury')
-	.controller('jury.TasksController', ["$scope", "Task", function($scope, Task){
+	.controller('jury.TasksController', ["$scope", "Task", "socket", "$sce", function($scope, Task, socket, $sce){
 		$scope.tasks = Task.query();
+
+		$scope.contests = [];
+		$scope.contest = {};
+
+		socket.get('/contest', {}, function(data){
+			$scope.contests.push.apply($scope.contests, data);
+		});
+
+		$scope.trustAsHtml = function(value) {
+			return $sce.trustAsHtml(value);
+		};
 	}]);
