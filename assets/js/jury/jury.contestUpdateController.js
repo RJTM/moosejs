e4415db772952cd19f9481aaa5b9a25638a9f753
Emilio Tirado <emilio.tirado57@gmail.com>
2015-02-17 14:27:59 -0430

    bugfix with dates in jury views

--- assets/js/jury/jury.contestUpdateController.js
+++ assets/js/jury/jury.contestUpdateController.js
@@ -3,26 +3,46 @@
 angular.module('mooseJs.jury')
 	.controller('jury.ContestUpdateController', ["$scope","$state","$stateParams", "socket",  function($scope, $state, $stateParams, socket){
 		socket.get('/contest/'+$stateParams.id, function(data){
-			console.log(data);
+			var start = new Date(data.startTime),
+				end = new Date(data.endTime),
+				freeze = new Date(data.freezeTime),
+				unfreeze = new Date(data.unfreezeTime);
+			
+			data.startTime = new Date();
+			data.startTime.setTime(start.getTime());
+			data.endTime = new Date();
+			data.endTime.setTime(end.getTime());
+			data.freezeTime = new Date();
+			data.freezeTime.setTime(freeze.getTime());
+			data.unfreezeTime = new Date();
+			data.unfreezeTime.setTime(unfreeze.getTime());
+
 			$scope.contest = data;
 		});
 
-		$scope.contest = {};
+		$scope.contest = {
+			startTime : new Date(),
+			endTime : new Date(),
+			freezeTime : new Date(),
+			unfreezeTime : new Date()
+		}
 
 		$scope.$watch('contest.startTime', function(){
-			var start = new Date($scope.contest.startTime),
-				freeze = new Date($scope.contest.freezeTime),
-				unfreeze = new Date($scope.contest.unfreezeTime),
-				end = new Date($scope.contest.endTime);
+			if($scope.contest){
+				var start = $scope.contest.startTime.getTime(),
+					freeze = $scope.contest.freezeTime.getTime(),
+					unfreeze = $scope.contest.unfreezeTime.getTime(),
+					end = $scope.contest.endTime.getTime();
 
-			if(!$scope.contest.freezeTime || freeze < start)
-				$scope.contest.freezeTime = $scope.contest.startTime;
+				if(!$scope.contest.freezeTime || freeze < start)
+					$scope.contest.freezeTime = $scope.contest.startTime;
 
-			if(!$scope.contest.endTime || end < start)
-				$scope.contest.endTime = $scope.contest.startTime;
+				if(!$scope.contest.endTime || end < start)
+					$scope.contest.endTime = $scope.contest.startTime;
 
-			if(!$scope.contest.unfreezeTime || unfreeze < start)
-				$scope.contest.unfreezeTime = $scope.contest.startTime;
+				if(!$scope.contest.unfreezeTime || unfreeze < start)
+					$scope.contest.unfreezeTime = $scope.contest.startTime;
+			}
 		});
 
 		$scope.update = function(){
--- assets/js/jury/wizard.controller.js
+++ assets/js/jury/wizard.controller.js
@@ -1,24 +1,49 @@
 'use strict';
 
 angular.module('mooseJs.jury')
-	.controller('jury.WizardController', ["$scope", "$stateParams", "$state", "Contest", function($scope, $stateParams, $state, Contest){
-		$scope.contest = Contest.get({id: $stateParams.id});
+	.controller('jury.WizardController', ["$scope", "$stateParams", "$state", "socket", function($scope, $stateParams, $state, socket){
+		socket.get('/contest/'+$stateParams.id, function(data){
+			var start = new Date(),
+				end = new Date(),
+				freeze = new Date(),
+				unfreeze = new Date();
+			
+			data.startTime = new Date();
+			data.startTime.setTime(start.getTime());
+			data.endTime = new Date();
+			data.endTime.setTime(end.getTime());
+			data.freezeTime = new Date();
+			data.freezeTime.setTime(freeze.getTime());
+			data.unfreezeTime = new Date();
+			data.unfreezeTime.setTime(unfreeze.getTime());
+
+			$scope.contest = data;
+		});
+
+		$scope.contest = {
+			startTime : new Date(),
+			endTime : new Date(),
+			freezeTime : new Date(),
+			unfreezeTime : new Date()
+		}
 
 		$scope.$watch('contest.startTime', function(){
-			var start = new Date($scope.contest.startTime),
-				freeze = new Date($scope.contest.freezeTime),
-				unfreeze = new Date($scope.contest.unfreezeTime),
-				end = new Date($scope.contest.endTime);
+			if($scope.contest){
+				var start = $scope.contest.startTime.getTime(),
+					freeze = $scope.contest.freezeTime.getTime(),
+					unfreeze = $scope.contest.unfreezeTime.getTime(),
+					end = $scope.contest.endTime.getTime();
 
-			if(!$scope.contest.freezeTime || freeze < start)
-				$scope.contest.freezeTime = $scope.contest.startTime;
+				if(!$scope.contest.freezeTime || freeze < start)
+					$scope.contest.freezeTime = $scope.contest.startTime;
 
-			if(!$scope.contest.endTime || end < start)
-				$scope.contest.endTime = $scope.contest.startTime;
+				if(!$scope.contest.endTime || end < start)
+					$scope.contest.endTime = $scope.contest.startTime;
 
-			if(!$scope.contest.unfreezeTime || unfreeze < start)
-				$scope.contest.unfreezeTime = $scope.contest.startTime;
-		});
+				if(!$scope.contest.unfreezeTime || unfreeze < start)
+					$scope.contest.unfreezeTime = $scope.contest.startTime;
+			}
+		});	
 
 		$scope.next = function(level, taskIndex, subtaskIndex){
 			if(level === 'contest'){
